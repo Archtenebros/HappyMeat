e50b5144bef8d3d3339dbd54ac4bfbb54fc4173f
Arthur LEFEVRE <arthur.lefevre@etudiant.univ-reims.fr>
2018-05-20 13:28:33 +0200

    Generation of form from entity

---
+++ src/AppBundle/Form/NewsType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class NewsType extends AbstractType
+{
+/**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\News'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'appbundle_news';
+    }
+
+
+}
