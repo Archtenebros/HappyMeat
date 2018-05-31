80e6ce1112404b59fe94089ee8d7ded0787cab55
Arthur LEFEVRE <arthur.lefevre@etudiant.univ-reims.fr>
2018-06-01 00:31:38 +0200

    ProfileController

--- src/AppBundle/Controller/ProfileController.php
+++ src/AppBundle/Controller/ProfileController.php
@@ -6,29 +6,39 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class ProfileController extends Controller
 {
-    public function informationAction()
+    public function informationAction($id)
     {
-        return $this->render('@App/profile/information.html.twig', array());
+        return $this->render('@App/profile/information.html.twig', array(
+            "owner" => $this->getDoctrine()->getRepository("AppBundle:Owner")->find($id),
+        ));
     }
 
-    public function photosAction()
+    public function photosAction($id)
     {
-        return $this->render('@App/profile/photos.html.twig', array());
+        return $this->render('@App/profile/photos.html.twig', array(
+            "owner" => $this->getDoctrine()->getRepository("AppBundle:Owner")->find($id),
+        ));
     }
 
-    public function postsAction()
+    public function postsAction($id)
     {
-        return $this->render('@App/profile/posts.html.twig', array());
+        return $this->render('@App/profile/posts.html.twig', array(
+            "owner" => $this->getDoctrine()->getRepository("AppBundle:Owner")->find($id),
+        ));
     }
 
-    public function showAction()
+    public function showAction($id)
     {
-        return $this->render('@App/profile/show.html.twig', array());
+        return $this->render('@App/profile/show.html.twig', array(
+            "owner" => $this->getDoctrine()->getRepository("AppBundle:Owner")->find($id),
+        ));
     }
 
-    public function streamAction()
+    public function streamAction($id)
     {
-        return $this->render('@App/profile/stream.html.twig', array());
+        return $this->render('@App/profile/stream.html.twig', array(
+            "owner" => $this->getDoctrine()->getRepository("AppBundle:Owner")->find($id),
+        ));
     }
 
     public function myproductsAction()
