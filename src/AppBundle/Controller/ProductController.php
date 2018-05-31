ce95b802e721f28aa8715106693b0f69d91bee25
Arthur LEFEVRE <arthur.lefevre@etudiant.univ-reims.fr>
2018-06-01 00:29:42 +0200

    ProductController

--- src/AppBundle/Controller/ProductController.php
+++ src/AppBundle/Controller/ProductController.php
@@ -8,11 +8,18 @@ class ProductController extends Controller
 {
     public function showAction()
     {
-        return $this->render('@App/product/show.html.twig', array());
+        //TODO Search Form
+        //Temporary return
+        $products = $this->getDoctrine()->getRepository("AppBundle:Animal")->findAll();
+        return $this->render('@App/product/show.html.twig', array(
+            "products" => $products,
+        ));
     }
 
-    public function showDetailsAction()
+    public function showDetailsAction($id)
     {
-        return $this->render('@App/product/showdetails.html.twig', array());
+        return $this->render('@App/product/showdetails.html.twig', array(
+            $this->getDoctrine()->getRepository("AppBundle:Animal")->find($id),
+        ));
     }
 }
