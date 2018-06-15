c56118d1b2fac9d092c9e531ecb6f94d25c00cb1
Arthur LEFEVRE <arthur.lefevre@etudiant.univ-reims.fr>
2018-06-15 14:33:45 +0200

    Start conversation API

--- src/AppBundle/Controller/MessageController.php
+++ src/AppBundle/Controller/MessageController.php
@@ -8,6 +8,7 @@ use AppBundle\Form\MessageType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Finder\Exception\AccessDeniedException;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class MessageController extends Controller
 {
@@ -54,5 +55,41 @@ class MessageController extends Controller
             "messages" => $messages,
             "form" => $form
         ));
+    }
+
+    /**
+     * Built like an api
+     *
+     * @param Request $request
+     * @param $idReceiver
+     * @return Response 500 if user not found, 403 if user not logged, 200 if message sent
+     */
+    public function startConversationAction(Request $request, $idReceiver)
+    {
+        $receiver = $this->getDoctrine()->getRepository('AppBundle:User')->find($idReceiver);
+
+        if($receiver == null)
+            return new Response("User not found !", 500);
+
+        $user = $this->getUser();
+
+        if($user == null)
+            return new Response("User not authenticated !", 403);
+
+        $sender = $this->get('fos_message.sender');
+        $repository = $this->get('fos_message.repository');
+        $conversations = $repository->getPersonConversations($this->getUser());
+        $thread = null;
+        foreach ($conversations as $conversation)
+            /** @var \FOS\Message\Model\Conversation $conversation */
+            if($conversation->isPersonInConversation($receiver))
+                $thread = $conversation;
+
+        if($thread == null)
+            $sender->startConversation($user, $receiver, $request->get('content'));
+        else
+            $sender->sendMessage($thread, $user, $request->get('content'));
+
+        return new Response('Message sent !', 200);
     }
 }
