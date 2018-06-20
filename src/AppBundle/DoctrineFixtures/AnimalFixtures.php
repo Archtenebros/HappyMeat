6d0d2ea73c8b9cdf37b32f30b1c78ca95012af50
Arthur LEFEVRE <arthur.lefevre@etudiant.univ-reims.fr>
2018-06-20 23:21:56 +0200

    Animal fixtures

--- src/AppBundle/DoctrineFixtures/AnimalFixtures.php
+++ src/AppBundle/DoctrineFixtures/AnimalFixtures.php
@@ -23,14 +23,37 @@ class AnimalFixtures extends Fixture implements OrderedFixtureInterface
         $animal1->setAuthor($this->getReference('jimmy'));
         $animal1->setTitle('Turtle');
         $animal1->setContent('This is a turtle');
+        $animal1->setName("Franklin");
+        $animal1->setAge(2);
+        $animal1->setWeight(125.5);
+
+        $animal2 = new Animal();
+        $animal2->setDate(new \DateTime());
+        $animal2->setAuthor($this->getReference('jimmy'));
+        $animal2->setTitle('Cow');
+        $animal2->setContent('This is a cow');
+        $animal2->setName("Bessie");
+        $animal2->setAge(3);
+        $animal2->setWeight(77);
+
+        $animal3 = new Animal();
+        $animal3->setDate(new \DateTime());
+        $animal3->setAuthor($this->getReference('nathan'));
+        $animal3->setTitle('Goat');
+        $animal3->setContent('This is a goat');
+        $animal3->setName("Billy");
+        $animal3->setAge(5);
+        $animal3->setWeight(50);
 
         $manager->persist($animal1);
+        $manager->persist($animal2);
+        $manager->persist($animal3);
         $manager->flush();
     }
 
     public function getOrder()
     {
-        // TODO: Implement getOrder() method.
+        return 5;
     }
 
 }
